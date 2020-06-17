893a994867294dbdddb7af3bf5fb76487a5d5671
xnacly <matteogropp@gmail.com>
2020-06-17 06:23:32 +0200

    more

--- redditer.py
+++ redditer.py
@@ -1,27 +1,32 @@
-from PIL import Image
-import requests
 from io import StringIO, BytesIO
+from PIL import Image
+import instaWrapper
+import requests
 
-class Reddit:
-	def __init__(self, subreddits):
+class BOT:
+	def __init__(self, subreddits, auth):
 		self.subreddits = subreddits
+		self.API = instaWrapper.InstagramBot(auth)
 		self.size = (1080,1080)
 
 	def requestRandom(self):
-		pass
+		return "https://i.redd.it/vx275wl93b551.jpg"
 
 	def requestBuffer(self, link):
-		r = requests.get(link)#"https://i.redd.it/vx275wl93b551.jpg")
+		r = requests.get(link)
 		return r.content
 
 	def resizeBuffer(self, Buffer):
 		img = Image.open(BytesIO(Buffer))
 		img = img.resize(self.size, Image.ANTIALIAS)
 		return img
-		# img.show()
+
+	def post(self, buffer, caption):
+		pass
 
 
-
-Red = Reddit("idk")
-buffer = Red.requestBuffer()
+with open("config.json") as f:
+	config = f.read()
+Red = BOT([], config)
+buffer = Red.requestBuffer(Red.requestRandom())
 Red.resizeBuffer(buffer)
